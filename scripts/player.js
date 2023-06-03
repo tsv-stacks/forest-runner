3324ef516d24655c0eb3eb4ee8f5a92023317e8d
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-06-03 18:44:11 +0100

    added limits on horizontal movement

--- scripts/player.js
+++ scripts/player.js
@@ -6,12 +6,25 @@ class Player {
     this.x = 0;
     this.y = this.game.height - this.height;
     this.image = document.getElementById("player");
+    this.speed = 0;
+    this.maxSpeed = 3;
   }
   update(input) {
+    this.x += this.speed;
+    // horizontal movement
     if (input.includes("ArrowRight")) {
-      this.x++;
+      this.speed = this.maxSpeed;
     } else if (input.includes("ArrowLeft")) {
-      this.x--;
+      this.speed = -this.maxSpeed;
+    } else {
+      this.speed = 0;
+    }
+
+    if (this.x < 0) {
+      this.x = 0;
+    }
+    if (this.x > this.game.width - this.width) {
+      this.x = this.game.width - this.width;
     }
   }
   draw(context) {
