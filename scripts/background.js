935759dba43bf18141a3e7409ef2e21b896c3b6e
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-06-04 21:01:46 +0100

    setup branch

---
+++ scripts/background.js
@@ -0,0 +1,13 @@
+class Layer {
+  constructor(game, width, height, speed, image) {
+    this.game = game;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.image = image;
+    this.x = 0;
+    this.y = 0;
+  }
+  update() {}
+  draw() {}
+}
